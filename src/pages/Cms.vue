0466da9166d1c093ae251833123702c62c6234dd
zStevenn <stevenli.inbox@gmail.com>
2023-02-03 11:41:54 +0100

    Added contentnav to cms

--- src/pages/Cms.vue
+++ src/pages/Cms.vue
@@ -1,7 +1,35 @@
 <template>
-  <h1>CMS</h1>
+  <div class="grid gap-8">
+    <top-navbar />
+    <content-navigation :items="items" />
+    <h1>CMS</h1>
+  </div>
 </template>
 
 <script>
-export default {};
+import TopNavbar from '../components/TopNavbar.vue';
+import ContentNavigation from '../components/ContentNavigation.vue';
+export default {
+  components: {
+    TopNavbar,
+    ContentNavigation,
+  },
+  data() {
+    return {
+      items: [
+        { id: 'sites', name: 'Sites' },
+        { id: 'cookieconsent', name: 'Cookie consent' },
+        { id: 'css', name: 'CSS' },
+        { id: 'js', name: 'JavaScripts' },
+        { id: 'menus', name: 'Menus' },
+        { id: 'layouts', name: 'Lay-outs' },
+        { id: 'webpagina', name: 'Webpaginas' },
+        { id: 'contents', name: 'Contents' },
+        { id: 'contentvariabel', name: 'Contentvariabelen' },
+        { id: 'acties', name: 'Acties' },
+        { id: 'voorwaardenset', name: 'Voorwaarden-sets' },
+      ],
+    };
+  },
+};
 </script>
